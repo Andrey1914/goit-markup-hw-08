a659103d59eb865ea537f166b5669218482fa66b
Andrey1914 <anlikajw@gmail.com>
2022-01-31 00:00:45 +0200

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
-(( ) => {
+(() => {
     const menuBtnRef = document.querySelector('[data-menu-btn]');
     const mobileMenuRef = document.querySelector('[data-menu]');
 
-    menuBtnRef.addEventListener('click', ( ) => {
+    menuBtnRef.addEventListener('click', () => {
         const expanded =
             menuBtnRef.getAttribute("aria-expended") === 'true' || false;
         
         menuBtnRef.classList.toggle('is-open');
-        menuBtnRef.setAttribute('aria-expended', !expended);
+        menuBtnRef.setAttribute('aria-expended', !expanded);
 
         mobileMenuRef.classList.toggle('is-open');
     });
-})( );
+})();
